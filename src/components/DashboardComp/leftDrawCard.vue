2b7c320e6ad9e3c32d5a26898ddac3280bbf13c8
namanapp8998 <namanchauhan8998@gmail.com>
2020-03-22 21:33:20 +0530

    LftDrwr-layout_LftCrd+hmPg+CrtAcc

--- src/components/DashboardComp/leftDrawCard.vue
+++ src/components/DashboardComp/leftDrawCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card class="my-card shadow-15 full-width" v-ripple style="border-radius: 19px; background-color: #0559FD">
+  <q-card class="my-card shadow-15 full-width"  v-ripple style="border-radius: 19px; background-color: #0559FD">
   <q-card-section horizontal>
     <q-card-section class="col-3 ">
     <circle-image :imgurl="imgurl"></circle-image>
